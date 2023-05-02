530e9e398e305a2b5a3c7daa80cefaa90da70912
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-05-02 14:13:07 +0200

    first Commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const team = [
+    {
+        nome : "Wayne Barnett",
+        ruolo : "Founder & CEO",
+        urlFoto : "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+    
+        nome : "Angela Caroll",
+        ruolo : "Chief Editor",
+        urlFoto : "angela-caroll-chief-editor.jpg",
+    
+    },
+    {
+        nome : "Walter Gordon",
+        ruolo : "Office Manager",
+        urlFoto : "walter-gordon-office-manager.jpg",
+    },
+    {
+        nome : "Angela Lopez",
+        ruolo : "Social Media Manager",
+        urlFoto : "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome : "Scott Estrada",
+        ruolo : "Developer",
+        urlFoto : "scott-estrada-developer.jpg",
+    },
+    {
+        nome : "Barbara Ramos",
+        ruolo : "Graphic Designer",
+        urlFoto : "barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+console.log(team);
